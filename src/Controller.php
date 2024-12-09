dfe02dee24fc2c64186ce0ab195c56f0bde4fc53
Dmitrii Kustov <simbiat@outlook.com>
2024-12-09 22:16:34 +0200

    Fix support for CTE

--- src/Controller.php
+++ src/Controller.php
@@ -1232,13 +1232,10 @@ class Controller
     private function isSelect(string $query, bool $throw = true): bool
     {
         #First check that whole text does not start with any of SELECT-like statements or with `WITH` (CTE)
-        if (preg_match('/\A\s*WITH/mui', $query) !== 1 && preg_match('/\A\s*('.implode('|', self::selects).')/mui', $query) !== 1) {
-            if ($throw) {
-                throw new \UnexpectedValueException('Query is not one of '.implode(', ', self::selects).'.');
-            }
-            return false;
-        }
-        if (preg_match('/^\s*(\(\s*)*('.implode('|', self::selects).')/mui', $query) === 1) {
+        if (preg_match('/\A\s*WITH/mui', $query) !== 1 &&
+            preg_match('/\A\s*('.implode('|', self::selects).')/mui', $query) !== 1 &&
+            preg_match('/^\s*(\(\s*)*('.implode('|', self::selects).')/mui', $query) === 1
+        ) {
             return true;
         }
         if ($throw) {
